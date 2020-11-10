8b62f8bae73cdbeace10eec469bbc96d7ee65d25
Eugene Kotov <mrkev2112@mail.ru>
2020-11-11 00:50:58 +0700

    Fix logic functionality

--- src/js/comments.js
+++ src/js/comments.js
@@ -1,79 +1,76 @@
 import { getComments } from "./service.js";
+import { sorter, dateFormatter } from "./helpers.js";
 
 let comments = [];
-let commentsContainer = document.querySelector("#comments-container");
 
-class Comment {
+const commentCounter = document.querySelector(".comment-counter");
+const commentsContainer = document.querySelector("#comments-container");
+
+export class Comment {
   constructor(comment) {
-    this.comment = comment.comment,
-    this.id = comment.id,
-    this.name = comment.name,
-    this.like = comment.like,
-    this.avatar = comment.avatar,
-    this.liked = false,
+    this.comment = comment.comment;
+    this.id = comment.id;
+    this.name = comment.name;
+    this.like = comment.like;
+    this.avatar = comment.avatar;
+    this.liked = false;
+    this.createdAt = dateFormatter(comment.createdAt);
 
-    this.render()
+    this.render();
   }
 
   render() {
-    const commentsWrapper = document.createElement('div')
-    const likes = document.createElement('div')
-
+    const commentsWrapper = document.createElement("div");
     commentsWrapper.innerHTML = this.template();
-    likes.innerHTML = this.likeTemplate()
-    commentsWrapper.classList.add("ui", "comment")
-    commentsWrapper.append(likes)
-    commentsContainer.append(commentsWrapper)
+    commentsWrapper.classList.add("ui", "comment");
+    commentsContainer.append(commentsWrapper);
 
-    let btn = likes.querySelector('.like-button')
-    btn.addEventListener('click', (e) => {
-      this.updateLike(e)
-    })
+    const btn = commentsWrapper.querySelector(".like-button");
+    btn.addEventListener("click", (e) => {
+      this.updateLike(e);
+    });
   }
 
   updateLike(e) {
-    let likeCounter = e.currentTarget.parentNode.querySelector('.comment-like')
-
-    e.currentTarget.classList.toggle('red');
-
-    (this.liked === false) ? this.like+=1 : this.like-=1
-    likeCounter.innerHTML = this.like
-    this.liked = !this.liked
+    let likeCounter = e.currentTarget.parentNode.querySelector(".comment-like");
+    e.currentTarget.classList.toggle("red");
+    this.liked === false ? (this.like += 1) : (this.like -= 1);
+    likeCounter.innerHTML = this.like;
+    this.liked = !this.liked;
   }
 
-  likeTemplate() {
-    return  `
-    <div class="actions">
-      <div class="ui labeled button" tabindex="0">
-      <div class="ui button like-button"><i class="heart icon"></i> Like </div>
-      <a class="ui basic red left pointing label comment-like">
-         ${this.like}
-      </a>
-      </div>
-      </div>
-
-      <div class="ui divider"></div>
-`
-  }
   template() {
     return `
     <a class="avatar" href=""> <img src="${this.avatar}"></a>
       <div class="content">
           <span class="author"> ${this.name} </span>
+          <div class="metadata">
+             <span class="date">${this.createdAt}</span>
+           </div>
           <div class="text"> ${this.comment} </div>
      </div>
+     <div class="actions ui right aligned container">
+       <div class="ui labeled button" tabindex="0">
+       <div class="ui animated button like-button" tabindex="0">
+         <div class="visible content"><i class="heart icon"></i>Like</div>
+         <div class="hidden content">
+           <i class="heart icon"></i>
+         </div>
+       </div>
+       <a class="ui basic red left pointing label comment-like">
+          ${this.like}
+       </a>
+       </div>
+       </div>
+       <div class="ui divider"></div>
     `;
   }
 }
 
-const setComments = (array) => {
+export const setComments = (array) => {
   array.map((item) => comments.push(new Comment(item)));
 };
 
-export const loadComments = () => {
-  getComments().then((data) => {
-    setComments(data.data);
-  });
+export const setComment = (comment) => {
+  new Comment(comment);
 };
-
-console.log(comments);
