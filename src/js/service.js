9d45b3eb4a0f01ba0a3183408daa5eec0bcf0f56
Eugene Kotov <mrkev2112@mail.ru>
2020-11-11 00:48:42 +0700

    Add api call

--- src/js/service.js
+++ src/js/service.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { randomPostId } from "./helpers.js";
 
 const commentApi = axios.create({
   baseURL: `https://5f996fae50d84900163b8a4f.mockapi.io/comments/`,
@@ -10,15 +11,8 @@ const commentApi = axios.create({
   timeout: 10000,
 });
 
-let randomPostId = () => {
-  var num = Math.floor(Math.random() * 10)
-  return num;
-};
+let randomNum = randomPostId();
 
-export const getComments = () => {
-  return commentApi.get("/comments");
-};
-
-const getPost = () => {
-  return commentApi.get("/post/" + randomPostId());
+export const getCardInfo = () => {
+  return commentApi.get("/post/" + randomNum);
 };
